703d1d4110e3178f5e3482bc28b2121201d4a305
Ricardo Rosa <rmsrosa@gmail.com>
2020-11-19 00:32:58 -0300

    set units from json exchange- rate files

--- src/UnitfulCurrencies.jl
+++ src/UnitfulCurrencies.jl
@@ -6,7 +6,8 @@ Module extending Unitful.jl with currencies.
 """
 module UnitfulCurrencies
 
-using Unitful
+using Unitful, JSON, Dates
+using Unitful: uconvert, @dimension, @unit, @refunit
 
 """
     ExchangeRate
@@ -17,14 +18,48 @@ abstract type ExchangeRate end
 
 Base.broadcastable(x::ExchangeRate) = Ref(x)
 
+# Define currency dimension
 
-# Define currencies
+@dimension  𝐂   "C"     Currency
 
-@dimension 𝐂    "C"         Currency
+# Load exchange rates
 
-@refunit USD    "USD\$"     USDollar            𝐂           false
-@unit BRA       "BR\$"      BrazilianReais      USD/5       false
+exr_dir = "src/exchange_rates/"
+jexr = Dict()
+for entry in readdir(exr_dir)
+    if entry[end-4:end] == ".json"
+        j = JSON.parsefile(exr_dir * entry)
+        jexr[j["date"]] = Dict("base" => j["base"], "rates" => j["rates"])
+    end
+end
 
+# Set date to initialize rates in the unit definitions
+date = "2020-02-01"
+
+# Set reference unit, base currency, and base factor
+@refunit    EUR     "EUR"       Euro                𝐂           false
+base_curr = "EUR"
+base_factor = jexr[date]["base"] == base_curr ? 1.0 : 1/jexr[date]["rates"][base_curr]
+
+# define units
+for (curr, rate) in jexr[date]["rates"]
+    if curr != base_curr
+        rate_to_base = base_factor * rate
+        eval(
+            quote
+                Unitful.@unit_symbols($curr,$curr,𝐂,(1/$rate_to_base, 1))
+                Unitful.abbr(::Unitful.Unit{Symbol($curr),𝐂}) = $curr
+            end
+            )
+    end
+end
+
+function set_exchange_rates(date::Union{String,Date})
+    if typeof(date) == String
+        date = Date(date)
+    end
+    date
+end
 
 # Register the above units and dimensions in Unitful
 __init__() = Unitful.register(UnitfulCurrencies)
