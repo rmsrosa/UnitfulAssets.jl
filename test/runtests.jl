1baab2522a1d142dd7029e72dbab9cd2b4ded8ee
Ricardo Rosa <rmsrosa@gmail.com>
2020-11-21 08:53:17 -0300

    rename folder with the exchange market files

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,10 +3,10 @@ using UnitfulCurrencies
 using Test
 
 @testset "Currencies" begin
-
+    # read exchange market from a fixer.io file
     fixer_mkt = Dict(
         "2020-11-01" => UnitfulCurrencies.get_fixer_mkt(
-            "test/exchange_rates/2020-11-01_fixer.json"
+            "test/exchange_markets/2020-11-01_fixer.json"
         )
     )
     # conversions
