053b46d1c7573d26f9ddcc3ebc1673285a873973
Ricardo Rosa <rmsrosa@gmail.com>
2020-11-30 18:53:23 -0300

    rename test_exchmkt to exch_mkt_27nov2020

--- test/runtests.jl
+++ test/runtests.jl
@@ -17,7 +17,7 @@ currencylayer_exchmkt = Dict(
     )
 )
 # rates on Nov 27, 2020, for testing purposes
-test_mkt = generate_exchmkt([
+exch_mkt_27nov2020 = generate_exchmkt([
     ("EUR","USD") => 1.19536, ("USD","EUR") => 0.836570,
     ("EUR","GBP") => 1.11268, ("GBP","EUR") => 0.898734,
     ("USD","CAD") => 1.29849, ("CAD","USD") => 0.770125,
@@ -45,17 +45,17 @@ BRLGBP_timeseries = Dict(
     @test uconvert(u"EUR", 1u"BRL", fixer_exchmkt["2020-11-01"], mode=-1) == 0.149575251159283u"EUR"
     @test uconvert(u"CAD", 1u"USD", currencylayer_exchmkt["2020-11-25"]) == 1.30045u"CAD"
     @test uconvert(u"BRL", 2u"hUSD", currencylayer_exchmkt["2020-11-25"]) == 1064.8188u"BRL"
-    @test uconvert(u"USD", 1u"EUR", test_mkt) == 1.19536u"USD"
-    @test uconvert(u"EUR", 1u"USD", test_mkt, mode=-1) == 0.8365680631776201u"EUR"
-    @test uconvert(u"CAD", 1u"EUR", test_mkt, mode=2) == 1.5521630063999998u"CAD"
-    @test uconvert(u"EUR", 1u"CAD", test_mkt, mode=-2) == 0.6442622301116068u"EUR"
-    @test uconvert(u"BRL", 1000u"CAD", test_mkt, mode=2) == 4111.674271249999u"BRL"
-    @test uconvert(u"BRL", 1000u"CAD", test_mkt, mode=-2) == 4111.6768608248185u"BRL"
+    @test uconvert(u"USD", 1u"EUR", exch_mkt_27nov2020) == 1.19536u"USD"
+    @test uconvert(u"EUR", 1u"USD", exch_mkt_27nov2020, mode=-1) == 0.8365680631776201u"EUR"
+    @test uconvert(u"CAD", 1u"EUR", exch_mkt_27nov2020, mode=2) == 1.5521630063999998u"CAD"
+    @test uconvert(u"EUR", 1u"CAD", exch_mkt_27nov2020, mode=-2) == 0.6442622301116068u"EUR"
+    @test uconvert(u"BRL", 1000u"CAD", exch_mkt_27nov2020, mode=2) == 4111.674271249999u"BRL"
+    @test uconvert(u"BRL", 1000u"CAD", exch_mkt_27nov2020, mode=-2) == 4111.6768608248185u"BRL"
     @test uconvert.(u"BRL", 1000u"GBP", values(BRLGBP_timeseries), mode=-1) ≈ [2591.68, 2891.26, 4018.00, 4743.38, 5850.35, 3333.56, 4140.27, 3912.06, 4430.86, 5323.68]u"BRL" (atol=0.01u"BRL")
-    @test typeof(@currency AAA TripleAs) <: Unitful.FreeUnits
+    @test typeof(UnitfulCurrencies.@currency AAA TripleAs) <: Unitful.FreeUnits
     @test_throws ArgumentError uconvert(u"EUR", 1u"BRL", fixer_exchmkt["2020-11-01"])
     @test_throws ArgumentError uconvert(u"CAD", 1u"BRL", fixer_exchmkt["2020-11-01"], mode=2)
-    @test_throws ArgumentError uconvert(u"EUR", 1u"CAD", test_mkt)
-    @test_throws ArgumentError uconvert(u"m",1u"km", test_mkt)
-    @test_throws ArgumentError @currency aaa tripleas
+    @test_throws ArgumentError uconvert(u"EUR", 1u"CAD", exch_mkt_27nov2020)
+    @test_throws ArgumentError uconvert(u"m",1u"km", exch_mkt_27nov2020)
+    @test_throws ArgumentError UnitfulCurrencies.@currency aaa tripleas
 end
