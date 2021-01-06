2351b2bf97df11b47bcf5605f0fe87d7675f2fb3
Ricardo Rosa <rmsrosa@gmail.com>
2021-01-06 19:54:49 -0300

    use up-to-date currencies from currency-iso.org

--- tools/generate_pkgdefauls.jl
+++ tools/generate_pkgdefauls.jl
@@ -1,14 +1,24 @@
 using DataFrames, CSV
 
-curr_list = CSV.File("tools/list_of_currencies.csv")
+df = CSV.read(joinpath("tools", "list_currency_iso.csv"), DataFrame, header=4)
 
-df_csv = curr_list |> DataFrame
+dropmissing!(df, "Alphabetic Code")
 
-nrows = size(df_csv)[1]
+nrows = size(df)[1]
 
-df_mod = DataFrame()
-df_mod.macrocall = fill("@currency", nrows)
-df_mod.code = df_csv.Code
-df_mod.abbr = df_csv.Currency
+#= df = combine(df, [Macro = fill("@currency", nrows)], "Alphabetic Code" => :Code,
+             :Currency => x -> replace.(titlecase.(x; strict=false), r" |’" => ""),
+             renamecols=false) =#
 
-CSV.write("src/pkgdefaults.jl", df_mod, delim="  ", header = false)
+df = crossjoin(DataFrame(Macro = fill("@currency", nrows)), combine(df, "Alphabetic Code" => :Code,
+             :Currency => x -> replace.(titlecase.(x; strict=false), r" |’" => ""),
+             renamecols=false))
+
+
+
+#= df_mod_new = DataFrame()
+df_mod_new.macrocall = fill("@currency", nrows)
+df_mod_new.abbr = df_curr_new[:,:Code]
+df_mod_new.code = df_curr_new[:,:Currency] =#
+
+CSV.write("src/pkgdefaults.jl", df, delim="  ", header = false)
