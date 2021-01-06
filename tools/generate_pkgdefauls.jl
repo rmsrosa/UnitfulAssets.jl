66ae306dd7b3e801f9aa48bfdf130ee803444ec0
Ricardo Rosa <rmsrosa@gmail.com>
2021-01-06 19:55:33 -0300

    cleaning up

--- tools/generate_pkgdefauls.jl
+++ tools/generate_pkgdefauls.jl
@@ -6,19 +6,8 @@ dropmissing!(df, "Alphabetic Code")
 
 nrows = size(df)[1]
 
-#= df = combine(df, [Macro = fill("@currency", nrows)], "Alphabetic Code" => :Code,
-             :Currency => x -> replace.(titlecase.(x; strict=false), r" |’" => ""),
-             renamecols=false) =#
-
 df = crossjoin(DataFrame(Macro = fill("@currency", nrows)), combine(df, "Alphabetic Code" => :Code,
              :Currency => x -> replace.(titlecase.(x; strict=false), r" |’" => ""),
              renamecols=false))
-
-
-
-#= df_mod_new = DataFrame()
-df_mod_new.macrocall = fill("@currency", nrows)
-df_mod_new.abbr = df_curr_new[:,:Code]
-df_mod_new.code = df_curr_new[:,:Currency] =#
 
 CSV.write("src/pkgdefaults.jl", df, delim="  ", header = false)
