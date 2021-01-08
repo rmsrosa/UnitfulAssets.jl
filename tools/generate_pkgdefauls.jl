674aa95d72652b5f2d206a94c8e9522095956db1
Ricardo Rosa <rmsrosa@gmail.com>
2021-01-07 22:38:44 -0300

    fix script that generates currency creation file

--- tools/generate_pkgdefauls.jl
+++ tools/generate_pkgdefauls.jl
@@ -3,9 +3,9 @@ using DataFrames, CSV
 df = CSV.read(joinpath("tools", "list_currency_iso.csv"), DataFrame, header=4)
 
 dropmissing!(df, "Alphabetic Code")
+unique!(df, :Currency)
 
 nrows = size(df)[1]
-
 df = hcat(DataFrame(Macro = fill("@currency", nrows)), combine(df, "Alphabetic Code" => :Code,
              :Currency => x -> replace.(titlecase.(x; strict=false), r" |’" => ""),
              renamecols=false))
